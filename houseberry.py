de220980bebaf9ed6643dfeb6c9518fbef45caba
Dandresfs <=>
2015-09-26 21:40:45 -0500

    Numeros permitidos

--- houseberry.py
+++ houseberry.py
@@ -1,3 +1,3 @@
 from run import YowsupEchoStack as wapp
-w = wapp(("573103149640","nfk2SXahAd7ADO5bZT6gsi61xt0="),encryptionEnabled = True)
+w = wapp(("573103149640","bgCpFv/dFSLL/Emp0yXBGGby8pk="),encryptionEnabled = True)
 w.start()
