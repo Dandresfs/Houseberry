eeba4eed941aca25a9a5b01b20b8845e4491b6f3
Dandresfs <dandresfsoto@gmail.com>
2015-09-27 08:50:25 +0000

    Luces completas

--- layer.py
+++ layer.py
@@ -1,5 +1,27 @@
+#/usr/bin/env python
+#-*- coding: utf-8 -*-
 from yowsup.layers.interface                           import YowInterfaceLayer, ProtocolEntityCallback
 from yowsup.layers.protocol_messages.protocolentities import TextMessageProtocolEntity
+import RPi.GPIO as GPIO
+import time
+comando = {"encender habitación principal":{"numero":[21],"estado":1,"mensaje":"Luz de la habitación principal encendida"},
+	"apagar habitación principal":{"numero":[21],"estado":0,"mensaje":"Luz de la habitación principal apagada"},
+           "encender baño principal":{"numero":[20],"estado":1,"mensaje":"Luz del baño principal encendida"},
+           "apagar luz baño principal":{"numero":[20],"estado":0,"mensaje":"Luz del baño principal apagada"},
+           "encender luz entrada":{"numero":[16],"estado":1,"mensaje":"Luz de la entrada encendida"},
+           "apagar luz entrada":{"numero":[16],"estado":0,"mensaje":"Luz de la entrada apagada"},
+           "encender luz sala":{"numero":[12],"estado":1,"mensaje":"Luz de la sala encendida"},
+           "apagar luz sala":{"numero":[12],"estado":0,"mensaje":"Luz de la sala apagada"},
+           "encender luz habitación 1":{"numero":[25,23],"estado":1,"mensaje":"Luces de la habitación 1 encendidas"},
+           "apagar luz habitación 1":{"numero":[25,23],"estado":0,"mensaje":"Luces de la habitación 1 apagadas"},
+           "encender baño":{"numero":[18],"estado":1,"mensaje":"Luz del baño encendida"},
+           "apagar baño":{"numero":[18],"estado":0,"mensaje":"Luz del baño apagada"},
+           "encender luz habitación 2":{"numero":[24],"estado":1,"mensaje":"Luz de la habitación 2 encendida"},
+           "apagar luz habitación 2":{"numero":[24],"estado":0,"mensaje":"Luz de la habitación 2 apagada"},
+           "encender todas":{"numero":[21,20,16,12,25,23,18,24],"estado":1,"mensaje":"Todas las luces de la casa se han encendido"},
+           "apagar todas":{"numero":[21,20,16,12,25,23,18,24],"estado":0,"mensaje":"Todas las luces de la casa se han apagado"},
+           }
+
 
 class EchoLayer(YowInterfaceLayer):
 
@@ -15,7 +37,6 @@ class EchoLayer(YowInterfaceLayer):
 
 
 
-
     @ProtocolEntityCallback("receipt")
     def onReceipt(self, entity):
         self.toLower(entity.ack())
@@ -26,10 +47,38 @@ class EchoLayer(YowInterfaceLayer):
         numeros = archivo.readlines()
         numeros = [numero.replace('\n', '') for numero in numeros]
         if len(filter(lambda x: str(messageProtocolEntity.getFrom(False)) in x, numeros)) > 0:
-            self.message_send(messageProtocolEntity.getFrom(False),"Estas registrado pero ahorita no joven")
+	    if messageProtocolEntity.getBody().lower() == "comandos":
+		comandos = "Encender/Apagar habitación principal\nEncender/Apagar baño principal\nEncender/Apagar luz entrada\nEncender/Apagar sala\nEncender/Apagar habitación 1\nEncender/Apagar baño\nEncender/Apagar habitación 2\nEncender/Apagar todas"
+                print("--------------------------------------------------------------------------------")
+		print("Solicitud de comandos:"+str(messageProtocolEntity.getFrom(False))+" - "+time.strftime("%I:%M:%S %p"))
+                print("--------------------------------------------------------------------------------")
+                self.message_send(messageProtocolEntity.getFrom(False),comandos)
+                
+		
+            else:
+		try:
+			accion = comando[messageProtocolEntity.getBody().lower()]
+		except:
+			print("--------------------------------------------------------------------------------")
+                        print("Comando inválido:"+str(messageProtocolEntity.getFrom(False))+" - "+time.strftime("%I:%M:%S %p"))
+                        print("--------------------------------------------------------------------------------")
+			self.message_send(messageProtocolEntity.getFrom(False),"Comando inválido")
+			return
+                
+                for numero in accion["numero"]:
+                    GPIO.output(numero,accion["estado"])
+                    
+		print("--------------------------------------------------------------------------------")
+		print("Comando: "+str(messageProtocolEntity.getBody())+" - Numero:" +str(messageProtocolEntity.getFrom(False))+" - "+time.strftime("%I:%M:%S %p"))
+                print("--------------------------------------------------------------------------------")
+		self.message_send(messageProtocolEntity.getFrom(False),accion["mensaje"])
+		
         else:
+            print("--------------------------------------------------------------------------------")
+            print("Numero no registrado:"+str(messageProtocolEntity.getFrom(False))+" - "+time.strftime("%I:%M:%S %p"))
+            print("--------------------------------------------------------------------------------")
             self.message_send(messageProtocolEntity.getFrom(False),"Tu numero no esta registrado")
-        print("Echoing %s to %s" % (messageProtocolEntity.getBody(), messageProtocolEntity.getFrom(False)))
+            
 
     def onMediaMessage(self, messageProtocolEntity):
         # just print info
--- run.py
+++ run.py
@@ -15,9 +15,11 @@ from yowsup.layers.protocol_calls              import YowCallsProtocolLayer
 from yowsup.common import YowConstants
 from yowsup import env
 import logging
+import RPi.GPIO as GPIO
 
 class YowsupEchoStack(object):
     def __init__(self, credentials, encryptionEnabled = False):
+	GPIO.cleanup()
         if encryptionEnabled:
             from yowsup.layers.axolotl                     import YowAxolotlLayer
             layers = (
@@ -46,6 +48,26 @@ class YowsupEchoStack(object):
 
     def start(self):
         self.stack.broadcastEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECT))
+	GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+	GPIO.setup(21, GPIO.OUT)	#Led 1
+	GPIO.setup(20, GPIO.OUT)	#Led 2
+	GPIO.setup(16, GPIO.OUT)        #Led 3
+	GPIO.setup(12, GPIO.OUT)        #Led 4
+	GPIO.setup(25, GPIO.OUT)        #Led 5
+	GPIO.setup(24, GPIO.OUT)        #Led 6
+	GPIO.setup(23, GPIO.OUT)        #Led 7
+	GPIO.setup(18, GPIO.OUT)        #Led 8
+
+	GPIO.output(21, GPIO.LOW)
+	GPIO.output(20, GPIO.LOW)
+	GPIO.output(16, GPIO.LOW)
+        GPIO.output(12, GPIO.LOW)
+	GPIO.output(25, GPIO.LOW)
+        GPIO.output(24, GPIO.LOW)
+	GPIO.output(23, GPIO.LOW)
+        GPIO.output(18, GPIO.LOW)
+
         try:
             self.stack.loop()
         except AuthError as e:
