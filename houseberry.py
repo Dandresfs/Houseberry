a79d7e88918b70047641d92034b815b350366daf
Diego Andres Fonseca Soto <dandresfs_04@hotmail.com>
2015-10-27 11:34:12 -0500

    Actualizacion codigo

--- houseberry.py
+++ houseberry.py
@@ -1,3 +1,3 @@
 from run import YowsupEchoStack as wapp
-w = wapp(("573103149640","bgCpFv/dFSLL/Emp0yXBGGby8pk="),encryptionEnabled = True)
+w = wapp(("573103149640","K4GLAIuFDqml2oLwslu2De81ntc="),encryptionEnabled = True)
 w.start()
